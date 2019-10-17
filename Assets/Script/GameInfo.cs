13cb112da8856b130a116a6faad2d09819f2673b
TAISHI SENOH <mokut2017@gmail.com>
2019-10-18 01:29:10 +0900

    ADD. 入力キーをログに表示する場合のスクリプト

---
+++ Assets/Script/GameInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            // 入力されたキー名
+            string keyStr = Input.inputString;
+            Debug.Log(keyStr);
+        }
+
+        foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(code))
+            {
+                // 入力されたキー名を表示
+                Debug.Log(code.ToString());
+            }
+        }
+
+
+
+    }
+}
